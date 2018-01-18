5ccf16fedeeb8a462023dba96e02feee4484eea0
Maximiliano Opitz Turra <Maximiliano@MacBook-Pro-de-Maximiliano.local>
2018-01-17 23:25:58 -0300

    makes the key of the push notification model a primary key

--- MDC/push_notification/models.py
+++ MDC/push_notification/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class PushNotificationToken(models.Model):
-    key = models.CharField(blank=False, max_length=300)
+    key = models.CharField(blank=False, max_length=300, primary_key=True, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
